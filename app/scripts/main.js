376a98337b5dd14f825b851e08c2ea80395cf181
Ayoze Barrera <ayozebarrera@gmail.com>
2015-02-24 12:15:11 +0000

    getting ready

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,66 @@
-/* jshint devel:true */
-console.log('\'Allo \'Allo!');
+var siteHandler = {
+  /**
+   * Establece los eventos de la app
+   */
+  setEvents: {
+    /**
+     * Al hacer click sobre una caja se muestra un modal con su contenido (items).
+     * Si la caja ha sido comprobada se ocultará al cerrar el modal.
+     */
+  },
+  init: function() {
+    console.log('init!');
+    //af9d9d26 municipios
+    L.mapbox.accessToken = 'pk.eyJ1Ijoiam9zZW5tYXIiLCJhIjoiRTBnY2plZyJ9.16b9pM_DfVdunAs6jZmE-A';
+
+    var southWest = L.latLng(-59.6, -180),
+        northEast = L.latLng(59.6, 180),
+        bounds = L.latLngBounds(southWest, northEast);
+
+    var map = L.mapbox.map('map', 'josenmar.3773a852', {
+      // set that bounding box as maxBounds to restrict moving the map
+      // see full maxBounds documentation:
+      // http://leafletjs.com/reference.html#map-maxbounds
+      maxBounds: bounds,
+      maxZoom: 6,
+      minZoom: 3,
+      tileLayer: {
+          // This map option disables world wrapping. by default, it is false.
+          continuousWorld: false,
+          // This option disables loading tiles outside of the world bounds.
+          noWrap: true
+      }
+    });
+    L.control.scale().addTo(map);
+
+    function addLayer(layer, name, zIndex) {
+        layer
+            .setZIndex(zIndex)
+            .addTo(map);
+
+        // Create a simple layer switcher that
+        // toggles layers on and off.
+        var link = document.createElement('a');
+            link.href = '#';
+            link.className = 'active';
+            link.innerHTML = name;
+
+        link.onclick = function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+
+            if (map.hasLayer(layer)) {
+                map.removeLayer(layer);
+                this.className = '';
+            } else {
+                map.addLayer(layer);
+                this.className = 'active';
+            }
+        };
+
+        layers.appendChild(link);
+    }
+  }
+};
+
+siteHandler.init();
